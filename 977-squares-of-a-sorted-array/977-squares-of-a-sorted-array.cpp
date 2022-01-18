86563018a43aa2495ecce7cb5f1d6e3824ff87e7
Tanmay Jena <tjena2@gmail.com>
2022-01-18 14:44:29 -0700

    Time: 73 ms (10.97%), Space: 25.8 MB (81.48%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        //bf
+        for(int i=0;i<nums.size();i++){
+            nums[i]*=nums[i];
+        }
+        
+        // for(auto i : nums){
+        //     cout << i << " ";
+        // }
+        
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
