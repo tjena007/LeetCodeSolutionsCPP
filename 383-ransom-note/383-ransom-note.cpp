d7b7a7bf3fb09c4bd1e875a139bb4464aa0aca61
Tanmay Jena <tjena2@gmail.com>
2022-01-27 16:37:00 -0700

    Time: 38 ms (14.78%), Space: 8.8 MB (40.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        bool ans = true;
-        
-        unordered_map<char,int> m;
-        
+        map<char,int> m;
         for(auto i: magazine){
             m[i]++;
         }
         
         for(auto i: ransomNote){
-            if(m[i] == 0) return false;
+            if(m[i] == 0){
+                return false;
+            }
             else{
                 m[i]--;
             }
         }
         
-        
-        return ans;
+        return true;
     }
 };
