5a1511244735e1882567ec4bdda0488a9eca2351
Tanmay Jena <tjena2@gmail.com>
2022-02-22 04:43:58 +0530

    Time: 28 ms (47.77%), Space: 19.4 MB (99.33%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int maj = nums[0],count =0;
+        for(auto i : nums){
+            if(i == maj) count++;
+            else{
+                count --;
+                if(count == 0){
+                    maj = i;
+                    count++;
+                }
+            }
+        }
         
-        return nums[nums.size()/2];
+        return maj;
     }
 };
