25437fed26ba4e53b0e078b051ae0a05e94d628c
Tanmay Jena <tjena2@gmail.com>
2022-03-13 01:24:28 -0700

    Time: 2 ms (42.21%), Space: 6.4 MB (10.36%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,33 +2,21 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        bool ans=true;
-        // if(s.size() == 1){
-        //     return false;
-        // }
-        for(auto i: s){
-            //cout << st.size() << endl;
-            if(st.size()!=0 && i=='}' && st.top()=='{'){
+        for(int i=0;i<s.size();i++){
+            if(!st.empty() && s[i] == ')' && st.top() == '('){
                 st.pop();
             }
-            else if(st.size()!=0 && i==')' && st.top()=='('){
+            else if(!st.empty() && s[i] == ']' && st.top() == '['){
                 st.pop();
             }
-            else if(st.size()!=0 && i==']' && st.top()=='['){
+            else if(!st.empty() && s[i] == '}' && st.top() == '{'){
                 st.pop();
             }
             else{
-                //cout << i << endl;
-                st.push(i);
+                st.push(s[i]);
             }
         }
         
-        if(st.size() != 0){
-            ans=false;
-        }
-        
-        
-        
-        return ans;
+        return st.empty();
     }
 };
