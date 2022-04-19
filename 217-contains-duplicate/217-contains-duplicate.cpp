28decd9b0ba53a6f491db8b6f863028fd28d146a
Tanmay Jena <tjena2@gmail.com>
2022-04-20 00:45:18 +0530

    Time: 120 ms (54.79%), Space: 52.1 MB (34.18%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         map<int,int> m;
-        for(auto i: nums){
-            m[i]+=1;
-        }
-        bool flag=false;
-        for(auto i: m){
-            if(i.second >1){
-                flag=true;
-                break;
+        
+        for(auto i : nums){
+            if(m[i]>0){
+                return true;
+            }
+            else{
+                m[i]++;
             }
         }
         
-        return flag;
+        return false;
     }
 };
