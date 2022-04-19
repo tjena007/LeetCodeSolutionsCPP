726ee0a3723bce6ccffad229e230074ce8e0386b
Tanmay Jena <tjena2@gmail.com>
2022-04-20 00:50:40 +0530

    Time: 98 ms (81.30%), Space: 51.5 MB (51.12%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int,int> m;
+        unordered_map<int,int> m;
         
         for(auto i : nums){
             if(m[i]>0){
