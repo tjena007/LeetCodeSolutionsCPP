4f1ac95c18a1fddd2274c523b542c734ae0011e2
Tanmay Jena <tjena2@gmail.com>
2022-04-27 03:01:38 +0530

    Time: 264 ms (21.33%), Space: 29.8 MB (14.06%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,34 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        set<vector<int>> se;
-        
         sort(nums.begin(),nums.end());
         
+        map<vector<int>,int> m;
+        
         for(int i=0;i<nums.size();i++){
-            //if(nums[i]>0) break;
-            int sum = 0 - nums[i];
-            int low=0,high = nums.size() -1;
+            if(nums[i]>0)break;
+            
+            int target = 0 - nums[i];
+            
+            int low=0,high=nums.size()-1;
+            
             while(low<high){
-                if(nums[low]+nums[high] == sum && (low!=i && high != i)){
-                    vector<int> s = {nums[low],nums[high],nums[i]};
-                    sort(s.begin(),s.end());
-                    se.insert(s);
-                    //ans.push_back(s);
-                    
-                    while (low<high && nums[low] == nums[low+1]){
-                        low++; 
-                    } 
-                    // to handle duplicates
-                    while (low<high && nums[high] == nums[high-1]) {
+                if(nums[low]+nums[high] == target){
+                    if(low == i){
+                        low++;
+                    }
+                    else if( high ==i){
                         high--;
                     }
+                    else{
+                        vector<int> temp = {nums[low],nums[high],nums[i]};
+                        sort(temp.begin(),temp.end());
+                        m[temp]++;
                         
-                    low++;
-                    high--;
+                        while (low<high && nums[low] == nums[low+1]){
+                        low++; 
+                        } 
+                        // to handle duplicates
+                        while (low<high && nums[high] == nums[high-1]) {
+                            high--;
+                        }
+                        
+                        high--;
+                        low++;
+                    }
                 }
-                else if(nums[low]+nums[high] > sum){
+                else if(nums[low]+nums[high] > target){
                     high--;
                 }
                 else{
@@ -36,12 +45,10 @@ public:
                 }
             }
         }
-        
-        for(auto i : se){
-            ans.push_back(i);
+        vector<vector<int>> ans;
+        for(auto i: m){
+            ans.push_back(i.first);
         }
-        
-       
         
         return ans;
         
