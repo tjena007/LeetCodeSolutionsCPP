c71d442118436469df3c38481ab42367f050a652
Tanmay Jena <tjena2@gmail.com>
2022-05-14 09:29:38 +0530

    Time: 156 ms (7.89%), Space: 32 MB (5.08%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans;
         
-        vector<string> s1 = strs;
-        
-        for(int i=0;i<strs.size();i++){
-            sort(strs[i].begin(),strs[i].end());
-        }
-        
-        // sort(strs.begin(),strs.end());
-        
-        map<string,vector<string>> m;
-        
-        for(int i=0;i<strs.size();i++){
-            m[strs[i]].push_back(s1[i]);
-        }
-        
-        for(auto i : m){
-            vector<string> temp;
-            for(int j=0;j<i.second.size();j++){
-                temp.push_back(i.second[j]);
+        unordered_map<string,vector<string>> m;
+        for(auto i : strs){
+            
+            vector<int> c(26,0);
+            for(auto j : i){
+                c[j-'a']++;
             }
             
-            ans.push_back(temp);
+            string key = "";
+            for(auto k : c){
+                key+= to_string(k) + '#';
+            }
+            
+            // cout << key << endl;
+            m[key].push_back(i);
+        }
+        
+        vector<vector<string>> ans;
+        
+        for(auto i : m){
+            vector<string> st;
+            for(auto j : i.second){
+                st.push_back(j);
+            }
+            ans.push_back(st);
         }
         
         
         return ans;
+        
     }
 };
