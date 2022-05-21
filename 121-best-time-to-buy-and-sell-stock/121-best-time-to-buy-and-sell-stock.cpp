aeb2036611111016ef1cf248f5b5ffff60051133
Tanmay Jena <tjena2@gmail.com>
2022-05-21 08:04:31 +0530

    Time: 160 ms (59.52%), Space: 93.4 MB (13.10%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-       stack<int> st;
-       int ans=INT_MIN;
-       vector<int> nums;
-       for(int i= prices.size()-1;i>=0;i--){
-           if(st.size() == 0){
-               nums.push_back(0);
-               st.push(prices[i]);
-           }
-           else if(st.top()<prices[i]){
-               nums.push_back(0);
-               st.push(prices[i]);
-           }
-           else if(st.top()>prices[i]){
-               nums.push_back(st.top());
-           }
-           else if(st.top() == prices[i]){
-               nums.push_back(0);
-           }
-       }
-        
-       
-        reverse(nums.begin(),nums.end());
-        
-        for(int i=0;i<prices.size();i++){
-            nums[i]-=prices[i];
+        if(prices.size() == 1){
+            return 0;
         }
         
-        for(auto i: nums){
-            //cout << i<< endl;
-            ans = max(ans,i);
-        }
-        if(ans<0){
-            ans=0;
+        int left = 0;
+        int right = 1;
+        
+        int mprofit = 0;
+        
+        while(right < prices.size()){
+            if(prices[left] > prices[right]){
+                left = right;
+                right++;
+            }else{
+                mprofit = max(mprofit, prices[right] - prices[left]);
+                right++;
+            }
         }
         
-       return ans;
+        return mprofit;
     }
 };
