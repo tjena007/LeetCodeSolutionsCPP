77d79e6352b0dc157de625652760f1499bf09399
Tanmay Jena <tjena@asu.edu>
2022-06-28 23:31:30 -0700

    Time: 190 ms (33.83%), Space: 26.2 MB (17.47%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,56 +1,31 @@
 class Solution {
 public:
+    vector<vector<int>> ans;
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         
-        map<vector<int>,int> m;
-        
         for(int i=0;i<nums.size();i++){
-            if(nums[i]>0)break;
-            
+            if(nums[i]>0) break;
             int target = 0 - nums[i];
-            
-            int low=0,high=nums.size()-1;
-            
-            while(low<high){
+            int low = i+1,high = nums.size()-1;
+            while(low < high){
                 if(nums[low]+nums[high] == target){
-                    if(low == i){
-                        low++;
-                    }
-                    else if( high ==i){
-                        high--;
-                    }
-                    else{
-                        vector<int> temp = {nums[low],nums[high],nums[i]};
-                        sort(temp.begin(),temp.end());
-                        m[temp]++;
-                        
-                        while (low<high && nums[low] == nums[low+1]){
-                        low++; 
-                        } 
-                        // to handle duplicates
-                        while (low<high && nums[high] == nums[high-1]) {
-                            high--;
-                        }
-                        
-                        high--;
-                        low++;
-                    }
-                }
-                else if(nums[low]+nums[high] > target){
+                    ans.push_back({nums[i],nums[low],nums[high]});
+                    
+                    while((low < high) && nums[low+1] == nums[low])low++;
+                    low++;
+                    while((low < high) && nums[high-1] == nums[high])high--;
                     high--;
                 }
-                else{
-                    low++;
-                }
+                else if(nums[low]+nums[high] > target) high--;
+                else low++;
             }
         }
-        vector<vector<int>> ans;
-        for(auto i: m){
-            ans.push_back(i.first);
-        }
+        
+        sort(ans.begin(),ans.end());
+        
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
         
         return ans;
-        
     }
 };
