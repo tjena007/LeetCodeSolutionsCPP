049167d2c91b80d42f3b8493b5ad4d293dd6c4e6
Tanmay Jena <tjena@asu.edu>
2023-07-31 18:14:17 -0500

    Time: 14 ms (77.76%), Space: 13.6 MB (9.95%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        map<int,bool> mrow;
-        map<int,bool> mcol;
+        unordered_set<int> setRows; 
+        unordered_set<int> setColumns; 
         
         int m = matrix.size(),n=matrix[0].size();
-        vector<vector<bool>> vis(m,vector<bool>(n,false));
+        //vector<vector<bool>> vis(m,vector<bool>(n,false));
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j] == 0 && !vis[i][j]){
-                    if(mrow[i] == false){
-                        for(int k=0;k<n;k++){
-                            if(matrix[i][k] != 0){
-                                matrix[i][k] = 0;
-                                vis[i][k] = true;
-                            } 
-                        }
-                        mrow[i] = true;
-                    }
-                    
-                    if(mcol[j] == false){
-                        for(int k=0;k<m;k++){
-                            if(matrix[k][j] != 0){
-                                matrix[k][j] = 0;
-                                vis[k][j] = true;
-                            }
-                        }
-                        mcol[j] = true;
-                    }
-                    vis[i][j] = true;
+                if(matrix[i][j] == 0){
+                    setRows.insert(i);
+                    setColumns.insert(j);
                 }
             }
         }
+        
+         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(setRows.count(i) > 0 || setColumns.count(j) > 0){
+                    matrix[i][j] = 0;
+                }
+            }
+         }
 
     }
 };
