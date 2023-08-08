4c76981f3a614146c068e29d6fcdb7be336ea072
Tanmay Jena <tjena@asu.edu>
2023-08-07 19:28:24 -0500

    Added solution - LeetHub

---
+++ Quick Sort - GFG/quick-sort.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
+    {
+        // code here
+        if(low < high){
+            int pIndex = partition(arr,low,high);
+            quickSort(arr,low,pIndex-1);
+            quickSort(arr,pIndex+1,high);
+        }
+    }
+    
+    public:
+    int partition (int arr[], int low, int high)
+    {
+       // Your code here
+       int pivot = arr[low];
+       int i=low,j=high;
+       while(i<j){
+           while(arr[i] <= pivot && i<=high-1){
+               i++;
+           }
+           while(arr[j]> pivot && j>=low+1){
+               j--;
+           }
+           if(i<j) swap(arr[i],arr[j]); //to check if indexes havent crossed yet
+            
+       }
+       swap(arr[low],arr[j]); //to put the pivot in its place
+       return j;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      Solution ob;
+    ob.quickSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}
+// } Driver Code Ends
