bcdcfdf0f88c1b9958e6793b53aa6bcccd9ee384
Tanmay Jena <tjena@asu.edu>
2023-08-13 19:04:47 -0500

    Time: 5 ms (38.42%), Space: 7.3 MB (30.48%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -4,5 +4,6 @@ public:
         return (str1 + str2 == str2 + str1)? 
         str1.substr(0, gcd(size(str1),size(str2))): "";
         
+        //direct math function works because we initially check if str1+ str2 == str2+str1
     }
 };
