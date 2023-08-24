6fb9a1c350c8588e57c809846b3beda5e0c49eed
Tanmay Jena <tjena@asu.edu>
2023-08-24 18:27:12 -0500

    Time: 3 ms (86.92%), Space: 10.6 MB (76.22%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-      sort(candidates.begin(), candidates.end());
-      vector <vector<int>> ans;
-      vector <int> ds;
-      findCombination(0, target, candidates, ans, ds);
-      return ans;
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        
+        vector<int> temp;
+        calc(0,candidates,temp,target,ans);
+        
+        return ans;
     }
     
-    void findCombination(int ind, int target, vector < int > & arr, vector < vector < int >> & ans, vector < int > & ds) {
-      if (target == 0) {
-        ans.push_back(ds);
-        return;
-      }
-      for (int i = ind; i < arr.size(); i++) {
-        if (i > ind && arr[i] == arr[i - 1]) continue;
-        if (arr[i] > target) break;
-        ds.push_back(arr[i]);
-        findCombination(i + 1, target - arr[i], arr, ans, ds);
-        ds.pop_back();
-      }
+    void calc(int idx,vector<int>& candidates, vector<int>& temp, int target,vector<vector<int>>& ans){
+        if(target == 0){
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i=idx;i<candidates.size();i++){
+            if(i>idx && candidates[i] == candidates[i-1]) continue;
+            if(candidates[i] > target) break;
+            
+            temp.push_back(candidates[i]);
+            calc(i+1,candidates,temp,target - candidates[i],ans);
+            temp.pop_back();
+        }
     }
 };
