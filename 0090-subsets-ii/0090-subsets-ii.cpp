6e9ef1a11e28088bf78561a5d2df75925fc10a47
Tanmay Jena <tjena@asu.edu>
2023-08-24 19:22:09 -0500

    Time: 0 ms (100.00%), Space: 7.4 MB (93.14%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> st;
+        vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         vector<int> temp;
         
-        calc(0,temp,nums,st);
-        
-        vector<vector<int>> ans;
-        for(auto i : st){
-            ans.push_back(i);
-        }
+        calc(0,temp,nums,ans);
         
         return ans;
     }
     
-    void calc(int idx,vector<int>& temp,vector<int>& nums, set<vector<int>>& st){
+    void calc(int idx,vector<int>& temp,vector<int>& nums, vector<vector<int>>& ans){
+        ans.push_back(temp);
         if(idx == nums.size()){
-            st.insert(temp);
             return;
         }
         
-        temp.push_back(nums[idx]);
-        calc(idx+1,temp,nums,st);
-        temp.pop_back();
-        calc(idx+1,temp,nums,st);
+        for(int i = idx;i<nums.size();i++){
+            if(i>idx && nums[i] == nums[i-1]) continue;
+            
+            temp.push_back(nums[i]);
+            calc(i+1,temp,nums,ans);
+            temp.pop_back();   
+        }
     }
 };
