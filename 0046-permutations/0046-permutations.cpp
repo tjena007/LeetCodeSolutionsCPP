6d6c36e3c2f40f8df3003b8078ea1e2e2c7c4094
Tanmay Jena <tjena@asu.edu>
2023-08-26 16:47:41 -0500

    Time: 0 ms (100.00%), Space: 7.6 MB (60.99%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        map<int,int> m;
-        vector<int> temp;
         vector<vector<int>> ans;
         
-        calc(nums, temp, m, ans);
+        calc(0,nums, ans);
         
         return ans;
     }
     
-    void calc(vector<int>& nums,vector<int>& temp,map<int,int>& m,vector<vector<int>>& ans){
-        if(temp.size() == nums.size()){
-            ans.push_back(temp);
+    void calc(int idx,vector<int>& nums,vector<vector<int>>& ans){
+        if(idx == nums.size()){
+            ans.push_back(nums);
             return;
         }
         
-        for(int i=0;i<nums.size();i++){
-            if(m[nums[i]] == 0){
-                m[nums[i]] = 1;
-                temp.push_back(nums[i]);
-                calc(nums, temp, m, ans);
-                temp.pop_back();
-                m[nums[i]] = 0;
-            }
+        for(int i=idx;i<nums.size();i++){
+            swap(nums[i],nums[idx]);
+            calc(idx+1,nums, ans);
+            swap(nums[i],nums[idx]);
         }
     }
 };
