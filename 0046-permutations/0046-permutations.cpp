8ffa0c340d562aa3a79ff47028814dcfd6fabfb2
Tanmay Jena <tjena@asu.edu>
2023-09-14 14:55:43 -0500

    Time: 0 ms (100.00%), Space: 8 MB (19.25%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
+        //keep element in the first position
+        //move the index to find all combinations with num in that place
+        //when idx reaches the end, add nums to ans
+        //swap them back to keep nums in its original place
         
-        calc(0,nums, ans);
+        helper(0,ans,nums); //to handle the backtracking
         
         return ans;
     }
     
-    void calc(int idx,vector<int>& nums,vector<vector<int>>& ans){
+    void helper(int idx,vector<vector<int>>& ans,vector<int>& nums){
         if(idx == nums.size()){
             ans.push_back(nums);
-            return;
         }
         
         for(int i=idx;i<nums.size();i++){
-            swap(nums[i],nums[idx]);
-            calc(idx+1,nums, ans);
-            swap(nums[i],nums[idx]);
+            swap(nums[idx],nums[i]);
+            helper(idx+1,ans,nums);
+            swap(nums[idx],nums[i]);
         }
     }
 };
