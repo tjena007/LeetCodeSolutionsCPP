1a2cc088f43358c3e1170c0eba0b81b1362afcde
Tanmay Jena <tjena@asu.edu>
2023-09-21 17:40:04 -0500

    Time: 2 ms (63.48%), Space: 8.6 MB (7.41%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+       map<int,int> m;
+       set<int> s;
+        
+       for(auto i : arr){
+           m[i]++;
+       }
+        
+       for(auto i : m){
+           s.insert(i.second);
+       }
+        
+       return m.size() == s.size();
+    }
+};
