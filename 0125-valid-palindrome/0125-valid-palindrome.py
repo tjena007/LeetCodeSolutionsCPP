f9ef8392b2e0da71293b9af3d71165aea2dd1623
Tanmay Jena <tjena@asu.edu>
2025-03-18 00:21:46 -0700

    Time: 11 ms (36.22%), Space: 17.9 MB (83.64%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        cleanString = "".join(ch for ch in s if ((ch >= "a" and ch <= "z")
-        or (ch >= "A" and ch <= "Z") or (ch >= "0" and ch <= "9"))).lower() 
-        print(cleanString)
+        l,r = 0,len(s) - 1
+        def valid(ch):
+            return ((ch >= "a" and ch <= "z") or (ch >= "A" and ch <= "Z") or (ch >= "0" and ch <= "9"))
 
-        return cleanString[::-1] == cleanString
+        while l <= r:
+            if valid(s[l]) == False:
+                l+= 1
+                continue
+            elif valid(s[r]) == False:
+                r -= 1
+                continue
+            elif s[l].lower() != s[r].lower():
+                return False
+            
+            l+=1
+            r-=1
+        
+        return True
