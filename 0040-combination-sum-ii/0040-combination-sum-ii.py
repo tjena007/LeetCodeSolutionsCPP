fb6aa111cf9687cd5bd09325ad61b4314b7c5318
Tanmay Jena <tjena@asu.edu>
2025-06-22 11:45:53 -0700

    Time: 31 ms (26.36%), Space: 18.1 MB (13.86%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.s = set()
+        candidates.sort()
+
+        def dfs(idx,curSum,arr):
+            if curSum == target:
+                self.s.add(tuple(arr))
+                return
+            
+            if idx == len(candidates) or curSum > target:
+                return
+
+            arr.append(candidates[idx])
+            dfs(idx+1,curSum + candidates[idx],arr)
+            arr.pop()
+
+
+            while idx+1 < len(candidates) and candidates[idx] == candidates[idx+1]:
+                idx = idx + 1
+
+            dfs(idx+1,curSum,arr)
+
+
+        dfs(0,0,[])
+
+        return [list(i) for i in self.s]
