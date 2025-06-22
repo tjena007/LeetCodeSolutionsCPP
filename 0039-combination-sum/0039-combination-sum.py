0a20d08a49f7e575fce919bc6fb370988720e4fa
Tanmay Jena <tjena@asu.edu>
2025-06-22 16:56:43 -0700

    Time: 19 ms (17.03%), Space: 18 MB (39.43%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,27 +1,23 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
-        self.s = set()
+        ans = []
 
         def dfs(arr,i,curSum):
-            # if arr == [2, 2, 3] or arr==[7]:
-            #     print(curSum,i)
             if curSum == target:
-                self.s.add(tuple(arr))
+                ans.append(arr.copy())
                 return
-            if i == len(candidates) or curSum > target or candidates[i] > target or curSum + candidates[i] > target :
+            if i == len(candidates) or curSum > target:
                 return
-            
-            
             
             # same index again
             arr.append(candidates[i])
             dfs(arr,i,curSum + candidates[i])
-            arr.pop() #needs some condition
+            arr.pop()
             # next index
             dfs(arr,i+1,curSum)
         
         dfs([],0,0)
 
-        return [list(i) for i in self.s]
+        return ans
         
