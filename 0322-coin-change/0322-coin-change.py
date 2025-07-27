6009ec3e09f2ecd945d7df9f8c4ada4b1a3e700d
Tanmay Jena <tjena@asu.edu>
2025-07-27 16:29:26 -0700

    Time: 715 ms (76.59%), Space: 18 MB (87.93%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,25 +1,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = {}
-        
-        def dfs(amount,idx):
-            if (amount,idx) in dp:
-                return dp[(amount,idx)]
-                
-            if amount == 0:
-                return 0
-            if idx == len(coins):
-                return float('inf')
-            
-            take = float('inf')
-            if coins[idx] <= amount:
-                take = 1 + dfs(amount - coins[idx],idx)
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
 
-            notTake = dfs(amount,idx + 1)
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min(dp[i], 1 + dp[i - coin])
 
-            dp[(amount,idx)] = min(take,notTake)
-
-            return dp[(amount,idx)]
-
-        ans = dfs(amount,0)
-        return -1 if ans == float('inf') else ans
+        return dp[amount] if dp[amount] != float('inf') else -1
