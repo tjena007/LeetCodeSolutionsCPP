e0e3c3f4f2fd81d64ff3a1acc1fdc786b2b13915
Tanmay Jena <tjena@asu.edu>
2025-08-05 07:45:24 -0700

    Time: 186 ms (21.85%), Space: 40.5 MB (5.27%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,34 +1,31 @@
 class MedianFinder:
-    #introduce two heaps, min and max
-    #count difference in both should not be greater than 1
-    #put in any one, then move the element to other heap if needed
-    # for median, if count is same, then both top element /2, or return top from which count is higher
+
     def __init__(self):
-        self.left,self.right = [],[]
+        self.minHeap = []  # higher half
+        self.maxHeap = []  # lower half (store as negative values)
 
     def addNum(self, num: int) -> None:
-        if self.right and num > self.right[0]:
-            heapq.heappush(self.right,num)
-        else:
-            heapq.heappush(self.left,-1*num)
-        
-        if len(self.left) > len(self.right) + 1:
-            val = -1 * heapq.heappop(self.left)
-            heapq.heappush(self.right,val)
-        if len(self.right) > len(self.left) + 1:
-            val = heapq.heappop(self.right)
-            heapq.heappush(self.left,-1*val)
+        # Step 1: Add to maxHeap (as negative)
+        heapq.heappush(self.maxHeap, -num)
+
+        # Step 2: Balance order (largest of maxHeap to minHeap)
+        if self.maxHeap and self.minHeap and (-self.maxHeap[0] > self.minHeap[0]):
+            val = -heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, val)
+
+        # Step 3: Balance sizes
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            val = -heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, val)
+
+        if len(self.minHeap) > len(self.maxHeap) + 1:
+            val = heapq.heappop(self.minHeap)
+            heapq.heappush(self.maxHeap, -val)
 
     def findMedian(self) -> float:
-        if len(self.left) > len(self.right):
-            return -1*self.left[0]
-        elif len(self.right) > len(self.left):
-            return self.right[0]
-        return (-1*self.left[0] + self.right[0])/2.0
-        
-
-
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
+        if len(self.maxHeap) == len(self.minHeap):
+            return (-self.maxHeap[0] + self.minHeap[0]) / 2
+        elif len(self.maxHeap) > len(self.minHeap):
+            return -self.maxHeap[0]
+        else:
+            return self.minHeap[0]
