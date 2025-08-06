5d7e997ac787335ad2e825b8b033dd504a6ad3e4
Tanmay Jena <tjena@asu.edu>
2025-08-06 12:44:53 -0700

    Time: 5669 ms (49.26%), Space: 19.1 MB (61.58%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -5,46 +5,43 @@ class TrieNode:
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        #build trie
-        def buildTrie(words):
-            root = TrieNode()
-            for word in words:
-                node = root
-                for ch in word:
-                    if ch not in node.children:
-                        node.children[ch] = TrieNode()
-                    node = node.children[ch]
-                node.word = word
-        
-            return root
-
-        result = []
-        def dfs(i,j,node):
-            char = board[i][j]
-            if char not in node.children:
-                return
-            
-            next_node = node.children[char]
-
-            if next_node.word:
-                result.append(next_node.word)
-                next_node.word = None 
-            
-            board[i][j] = '#'
-
-            for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                ni, nj = i + dx, j + dy
-                if 0 <= ni < len(board) and 0 <= nj < len(board[0]) and board[ni][nj] != '#':
-                    dfs(ni, nj, next_node)
-
-            board[i][j] = char  # Restore original character
-
-        
-        root = buildTrie(words)
+        self.result = []
+        self.board = board
+        root = self.buildTrie(words)
         
         for i in range(len(board)):
             for j in range(len(board[0])):
-                dfs(i, j, root)
-        return result
-
+                self.dfs(i, j, root)
         
+        return self.result
+
+    def buildTrie(self, words):
+        root = TrieNode()
+        for word in words:
+            node = root
+            for ch in word:
+                if ch not in node.children:
+                    node.children[ch] = TrieNode()
+                node = node.children[ch]
+            node.word = word
+        return root
+
+    def dfs(self, i, j, node):
+        char = self.board[i][j]
+        if char not in node.children:
+            return
+
+        next_node = node.children[char]
+
+        if next_node.word:
+            self.result.append(next_node.word)
+            next_node.word = None  # Avoid duplicates
+
+        self.board[i][j] = '#'  # Mark as visited
+
+        for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+            ni, nj = i + dx, j + dy
+            if 0 <= ni < len(self.board) and 0 <= nj < len(self.board[0]) and self.board[ni][nj] != '#':
+                self.dfs(ni, nj, next_node)
+
+        self.board[i][j] = char  # Restore original character
