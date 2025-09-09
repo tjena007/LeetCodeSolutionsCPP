b860d237c8f35bcd5271a74cfe07f84954fcf42f
Tanmay Jena <tjena@asu.edu>
2025-09-09 08:27:10 -0700

    Time: 90 ms (30.74%), Space: 32.9 MB (21.99%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,11 +3,7 @@ class Solution:
         currSum, ans = float('-inf'),float('-inf')
 
         for num in nums:
-            # print(num,':',currSum,ans)
-            if num > currSum:
-                currSum = max(num, currSum + num)
-            else:
-                currSum = currSum + num
+            currSum = max(num, currSum + num)
 
             ans = max(ans,currSum)
         
